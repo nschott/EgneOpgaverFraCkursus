e67aa8cf63b5f8937adbd7a3fdd7a9fc2178817f
Nicholas Schott <xns@e-boks.dk>
2019-01-14 15:52:42 +0100

    First change

--- opgave_variablertal/Program.cs
+++ opgave_variablertal/Program.cs
@@ -10,6 +10,31 @@ namespace opgave_variablertal
     {
         static void Main(string[] args)
         {
+            int heltal1 = 10;
+            heltal1++;
+            Console.WriteLine(heltal1);
+
+            heltal1--;
+            Console.WriteLine(heltal1);
+            
+            heltal1 += 20;
+            Console.WriteLine(heltal1);
+
+            double kommatal1 = 12.5;
+            kommatal1++;
+            Console.WriteLine(kommatal1);
+
+            kommatal1--;
+            Console.WriteLine(kommatal1);
+
+            kommatal1 *= 2;
+            Console.WriteLine(kommatal1);
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
         }
     }
 }
