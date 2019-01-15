634f5ca2b4dfc4b2ad70075fac2cce8d6a0576be
Nicholas Schott <xns@e-boks.dk>
2019-01-15 15:36:51 +0100

    Slut dag 2

---
+++ opgave_flow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace opgave_flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
