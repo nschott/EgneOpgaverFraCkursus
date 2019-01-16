45b11b48e1219af691bd0819c86b3a2081aae840
Nicholas Schott <xns@e-boks.dk>
2019-01-16 16:46:49 +0100

    Opgave_flow solved

--- opgave_flow/Program.cs
+++ opgave_flow/Program.cs
@@ -6,16 +6,47 @@ namespace opgave_flow
     {
         static void Main(string[] args)
         {
-            int visTal;
-            for (int x = 0; x < 10; x++)
+            int tal;
+            string visTal;
+            for (int x = 1; x < 11; x++)
             {
-                for (int y = 0; y < 10; y++)
+                for (int y = 1; y < 11; y++)
                 {
-                    visTal = y*x;
-                    Console.Write(visTal);
+                    tal = y*x;
+                    visTal = tal.ToString();
+                    if (tal > 50)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    
+                    Console.Write(visTal.PadLeft(4)+" ");
                 }
+                Console.WriteLine("");
             }
 
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Method 1");
+            for (int z = 0; z < 20; z+=3)
+            {
+                Console.WriteLine(z);
+                if (z == 15)
+                    break;
+            }
+
+            Console.WriteLine("Method 2");
+            for (int z = 0; z < 20; z++)
+            {
+                if (z % 3 == 0)
+                    Console.WriteLine(z);
+                else
+                    continue;
+                if (z == 15)
+                    break;
+            }
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 Console.Write("Press any key to continue . . . ");
